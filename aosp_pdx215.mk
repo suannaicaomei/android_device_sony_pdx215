85cc7511a50a7e325ed61c63fdbcd96b0cf99289
suannaicaomei <104017790+suannaicaomei@users.noreply.github.com>
2022-04-23 23:00:39 +0800

    Update aosp_pdx215.mk

--- aosp_pdx215.mk
+++ aosp_pdx215.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/sony/pdx215/device.mk)
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 PRODUCT_NAME := aosp_pdx215
 PRODUCT_DEVICE := pdx215
